a45fa44d43381f9653ab9243040a87587a358636
Alexey Zabashta <gwynblayde@mail.ru>
2021-10-17 22:26:13 +0300

    fix: add solution to binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -55,24 +55,101 @@ module.exports = class BinarySearchTree {
     }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return search(this.root1, data) 
+    function search(node, data){
+      if (!node){
+        return null;
+      }
+      if (node.data === data){
+        return node;
+      }
+
+      return data < node.data ?
+        search(node.left, data):
+        search(node.right, data);
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.root1=remNode(this.root1, data);
+
+    function remNode(node, data){
+      if (!node){
+        return null;
+      }
+      if (data <node.data){
+        node.left = remNode(node.left, data);
+        return node;
+      } else if (node.data < data){
+        node.right = remNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right){
+          return null;
+        }
+        if (!node.left){
+          node=node.right;
+          return node;
+        }
+        if (!node.right){
+          node=node.left;
+          return node;
+        }
+        let minFromRight = node.right;
+        while (minFromRight.left){
+          minFromRight=minFromRight.left;
+        }
+        node.data = minFromRight.data; 
+        node.right = remNode(node.right, minFromRight.data);
+        return node;      
+      }
+    }
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  min() {    
+    if (!this.root1){
+      return null;
+    }
+
+    let node = this.root1;
+    while (node.left){
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.root1){
+      return null;
+    }
+
+    let node = this.root1;
+    while (node.right){
+      node = node.right;
+    }
+    return node.data;
+  }
+
+  leftTrav(cb){
+    doLeft(this.root, cb);
+    function doLeft(node, cb){
+      if (node){
+        doLeft(node.left, cb);
+        cb(node.data);
+        doLeft(node.rigth,cb);
+      }
+    }
+  }
+  rightTrav(cb){
+    doRight(this.root, cb);
+    function doRight(node, cb){
+      if (node){
+        doRight(node.right, cb);
+        cb(node.data);
+        doRight(node.left,cb);
+      }
+    }
   }
 
 }
